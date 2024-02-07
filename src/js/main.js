c6d9cc2e09de97e30365ced6bc4e54ac0edab2eb
alextaz <alex.thazhmon@gmail.com>
2024-02-06 19:29:17 -0500

    Rudimentary system of balance
    - set up a system
    - created a balance file, this may be temporary
    - added a balance retriever
    - fake accounts
    - need to change balance file whenever changing accont details, not ideal

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,40 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import simpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
+// import simpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// get balance.json
+// fetch('./js/balance.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+//   });
+
+function getInformation(name, password) {
+    fetch('./js/balance.json')
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data);
+            const d = data[name];
+            // console.log(d);
+            if (d) {
+                if (d.password === password) {
+                    console.log('Login success');
+                    // window.location.href = 'balance.html';
+                    const balanceDiv = document.getElementById('balance');
+                    balanceDiv.innerHTML = "Balance: " + d.balance;
+                } else {
+                    alert('Password is incorrect');
+                }
+            } else {
+                alert('User not found');
+            }
+        });
+}
+
+document.getElementById('login-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    getInformation(name, password);
+});
